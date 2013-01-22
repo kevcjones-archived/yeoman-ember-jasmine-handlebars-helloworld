3327bfcba0b5847bb1088de0ebf25861cc71b6e2
Kevin Jones <him@kevincjones.co.uk>
2013-01-22 15:54:04 +0000

    tinkering some more

--- app/scripts/templates.js
+++ app/scripts/templates.js
@@ -3,7 +3,7 @@ helpers = helpers || Ember.Handlebars.helpers; data = data || {};
   var buffer = '', stack1, escapeExpression=this.escapeExpression;
 
 
-  data.buffer.push("<div class=\"container\">\n   <h1>Hurrah!!!</h1>\n   <p>You're up and running with Yeoman and Ember.js! Thank Fuck</p>\n</div>\n\n");
+  data.buffer.push("<div class=\"container\">\n   <h1>H1 Title</h1>\n   <h2>H2 Title</h2>\n   <h3>H3 Title</h3>\n   <p>The fox jumped over the sleep dog and then turned around and kicked him before making a run for it.</p>\n</div>\n\n");
   stack1 = helpers._triageMustache.call(depth0, "outlet", {hash:{},contexts:[depth0],data:data});
   data.buffer.push(escapeExpression(stack1) + "\n");
   return buffer;
